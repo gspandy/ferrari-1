687b991a87e99219ea7d7322061cb79fd0b107c4
xueli.xue <xueli.xue@dianping.com>
2015-12-31 17:54:09 +0800

    xxlJob更名为Ferrali

--- ferrari-admin-web/src/main/java/com/cip/ferrari/admin/core/model/FerraliJobLog.java
+++ ferrari-admin-web/src/main/java/com/cip/ferrari/admin/core/model/FerraliJobLog.java
@@ -3,7 +3,6 @@ package com.cip.ferrari.admin.core.model;
 import java.util.Date;
 
 /**
-<<<<<<< HEAD
  * ferrali log, used to track trigger process
  * @author xuxueli  2015-12-19 23:19:09
  */
@@ -96,100 +95,6 @@ public class FerraliJobLog {
 	@Override
 	public String toString() {
 		return "FerraliJobLog [id=" + id + ", jobName=" + jobName + ", jobCron=" + jobCron + ", jobClass=" + jobClass
-=======
- * xxl-job log, used to track trigger process
- * @author xuxueli  2015-12-19 23:19:09
- */
-public class FerraliJobLog {
-	
-	private int id;
-	// job info
-	private String jobName;
-	private String jobCron;
-	private String jobClass;
-	private String jobData;
-	
-	// trigger info
-	private Date triggerTime;
-	private String triggerStatus;
-	private String triggerMsg;
-	
-	// handle info
-	private Date handleTime;
-	private String handleStatus;
-	private String handleMsg;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getJobName() {
-		return jobName;
-	}
-	public void setJobName(String jobName) {
-		this.jobName = jobName;
-	}
-	public String getJobCron() {
-		return jobCron;
-	}
-	public void setJobCron(String jobCron) {
-		this.jobCron = jobCron;
-	}
-	public String getJobClass() {
-		return jobClass;
-	}
-	public void setJobClass(String jobClass) {
-		this.jobClass = jobClass;
-	}
-	public String getJobData() {
-		return jobData;
-	}
-	public void setJobData(String jobData) {
-		this.jobData = jobData;
-	}
-	public Date getTriggerTime() {
-		return triggerTime;
-	}
-	public void setTriggerTime(Date triggerTime) {
-		this.triggerTime = triggerTime;
-	}
-	public String getTriggerStatus() {
-		return triggerStatus;
-	}
-	public void setTriggerStatus(String triggerStatus) {
-		this.triggerStatus = triggerStatus;
-	}
-	public String getTriggerMsg() {
-		return triggerMsg;
-	}
-	public void setTriggerMsg(String triggerMsg) {
-		this.triggerMsg = triggerMsg;
-	}
-	public Date getHandleTime() {
-		return handleTime;
-	}
-	public void setHandleTime(Date handleTime) {
-		this.handleTime = handleTime;
-	}
-	public String getHandleStatus() {
-		return handleStatus;
-	}
-	public void setHandleStatus(String handleStatus) {
-		this.handleStatus = handleStatus;
-	}
-	public String getHandleMsg() {
-		return handleMsg;
-	}
-	public void setHandleMsg(String handleMsg) {
-		this.handleMsg = handleMsg;
-	}
-	
-	@Override
-	public String toString() {
-		return "XxlJobLog [id=" + id + ", jobName=" + jobName + ", jobCron=" + jobCron + ", jobClass=" + jobClass
->>>>>>> branch 'feature1223_infra' of http://code.dianpingoa.com/shop-business/ferrari.git
 				+ ", jobData=" + jobData + ", triggerTime=" + triggerTime + ", triggerStatus=" + triggerStatus
 				+ ", triggerMsg=" + triggerMsg + ", handleTime=" + handleTime + ", handleStatus=" + handleStatus
 				+ ", handleMsg=" + handleMsg + "]";
