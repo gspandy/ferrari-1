ba9a6a5d7eef94ee1f671dbf8b03f1ffafe46fdf
tengkai.yuan <tengkai.yuan@dianping.com>
2016-01-06 17:06:24 +0800

    去掉无用import

--- ferrari-admin-web/src/main/java/com/cip/ferrari/admin/core/util/HttpUtil.java
+++ ferrari-admin-web/src/main/java/com/cip/ferrari/admin/core/util/HttpUtil.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.StatusLine;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
